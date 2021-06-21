eb9bfb5409300ee5b77619a7736e416ef4e191b0
y4shiro <y4shironao@gmail.com>
2021-06-21 11:39:51 +0900

    gatsby-plugin-image を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: 'My First Gatsby Site',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+  ],
 };
